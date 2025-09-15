c63b3bc7f09fbfaca1a91dd69d8b9f8621804ccf
ftuncq <ftuncq@gmail.com>
2025-09-15 11:34:05 +0200

    fix bugs after recipe upgrade

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,7 +1,4 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-
-const app = startStimulusApp();
-import { startStimulusApp } from '@symfony/stimulus-bundle';
 import AddressAutocompleteController from './controllers/address_autocomplete_controller.js';
 import AppointmentTypeController from './controllers/appointment_type_controller.js';
 import AjaxFormController from './controllers/ajax_form_controller.js';
